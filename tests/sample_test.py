115c4c502805bcf43ff9c8a310ed5f157edde83f
Josef Kemetmüller <josef.kemetmueller@gmail.com>
2018-06-16 13:51:55 +0200

    Remove leftovers from alpha-version

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-import pytest
 from package_name.module_name import sum
+
 
 def test_sum():
     assert sum(1, 2) == 3
     assert sum(1, -1) == 0
     assert sum(1., -1.) < 1e-5
-
-if __name__ == '__main__':
-    unittest.main()
