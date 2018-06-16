b4bdf23ef16ff0351050972b33d5bc168ed897ac
Josef Kemetmüller <josef.kemetmueller@gmail.com>
2018-06-16 14:21:13 +0200

    Document usage for release.nix

--- release.nix
+++ release.nix
@@ -1,4 +1,8 @@
-{ nixpkgs, src }:
+# Build using:
+#   nix build -f release.nix
+{ nixpkgs ? <nixpkgs>
+, src ? builtins.fetchGit ./.
+}:
 rec {
   build-python2 = (import ./default.nix) {
     inherit nixpkgs src;
