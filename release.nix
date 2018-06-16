8aae46bb865785d3ee3d2fd6cf319c8b7792e0e7
Josef Kemetmüller <josef.kemetmueller@gmail.com>
2018-06-16 12:53:17 +0200

    Use nixpkgs-version defined by hydra
    Since hydra uses restricted-eval by default, we need to provide the
    nixpkgs version in hydra and cannot simply fetch the version specified
    in default.nix

--- release.nix
+++ release.nix
@@ -1,8 +1,11 @@
-{
+{ nixpkgs }:
+rec {
   build-python2 = (import ./default.nix) {
+    inherit nixpkgs;
     getPythonVersion = (p: p.python2Packages);
   };
   build-python3 = (import ./default.nix) {
+    inherit nixpkgs;
     getPythonVersion = (p: p.python3Packages);
   };
 }
